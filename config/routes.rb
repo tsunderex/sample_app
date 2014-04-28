18590fe72e547f10a36e39df803aeca1e56f023c
ccode <tsunderex@gmail.com>
2014-04-28 12:53:35 +0800

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 SampleApp::Application.routes.draw do
   get "static_pages/home"
   get "static_pages/help"
-  get "statis_pages/home"
-  get "statis_pages/help"
+  get "static_pages/about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
