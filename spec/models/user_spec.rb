94ed83da8fcc532675e6891e4b235a7f8e0f9c64
ccode <tsunderex@gmail.com>
2014-05-07 09:36:07 +0800

    exercise6

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -36,7 +36,7 @@ describe User do
   describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-                     foo@bar_baz.com foo@bar+baz.com]
+                     foo@bar_baz.com foo@bar+baz.com fii@bar..com]
       addresses.each do |invalid_address|
         @user.email = invalid_address
         expect(@user).not_to be_valid
@@ -46,7 +46,8 @@ describe User do
 
   describe "when email format is valid" do
     it "should be valid" do
-      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp 
+                     a+b@baz.cn]
       addresses.each do |valid_address|
         @user.email = valid_address
         expect(@user).to be_valid
@@ -61,8 +62,17 @@ describe User do
       user_with_same_email.save
     end
 
-    it { should_not be_valid }
-    
+    it { should_not be_valid }    
+  end
+
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
   end
 
   describe "when password is not present" do
@@ -75,12 +85,18 @@ describe User do
   end
 
   describe "when password doesn't match confirmation" do
-    before { @user.password_confirmation = "mismatch" }
+    before do
+      @user.password_confirmation = "mismatch"
+    end
+
     it { should_not be_valid }
   end
 
   describe "with a password that's too short" do
-    before { @user.password = @user.password_confirmation = "a" * 5 }
+    before do
+      @user.password = @user.password_confirmation = "a" * 5
+    end
+
     it { should be_invalid }
   end
 
